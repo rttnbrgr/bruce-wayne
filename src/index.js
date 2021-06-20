9e8cf6d647af4acc80102425c7deeb85c2b1bb4d
Nathan Rothenberger <nroth10@gmail.com>
2021-06-20 13:10:20 -0500

    add back js file + test out adding in the svgs dynamically

--- src/index.js
+++ src/index.js
@@ -7,3 +7,14 @@ foo();
 ["foo", "bar"].map(x => {
   console.log(`👀 ${x}`);
 });
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  //do work
+  const svgFillEl = document.getElementById("svgFill");
+  const svgFillMarkup = svgFillEl.outerHTML;
+  console.log("svgFillEl", svgFillEl);
+  svgFillEl.insertAdjacentHTML("afterend", svgFillMarkup);
+  svgFillEl.insertAdjacentHTML("afterend", svgFillMarkup);
+  svgFillEl.insertAdjacentHTML("afterend", svgFillMarkup);
+  svgFillEl.insertAdjacentHTML("afterend", svgFillMarkup);
+});
