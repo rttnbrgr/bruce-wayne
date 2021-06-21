493031fc403fbc5df07e42d7a97ac9b7d4d122a2
Nathan Rothenberger <nroth10@gmail.com>
2021-06-20 19:36:53 -0500

    wip: svg stuff

--- src/index.js
+++ src/index.js
@@ -12,9 +12,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
   //do work
   const svgFillEl = document.getElementById("svgFill");
   const svgFillMarkup = svgFillEl.outerHTML;
-  console.log("svgFillEl", svgFillEl);
-  svgFillEl.insertAdjacentHTML("afterend", svgFillMarkup);
-  svgFillEl.insertAdjacentHTML("afterend", svgFillMarkup);
-  svgFillEl.insertAdjacentHTML("afterend", svgFillMarkup);
-  svgFillEl.insertAdjacentHTML("afterend", svgFillMarkup);
+  const svgStrokeEl = document.getElementById("svgStroke");
+  console.log("svgStrokeEl", svgStrokeEl);
+  const svgStrokeEl2 = document.querySelector(".svg--stroke");
+  console.log("svgStrokeEl2", svgStrokeEl2);
+
+  const svgStrokeMarkup = svgStrokeEl2.outerHTML;
+  for (let i = 0; i < 3; i++) {
+    svgStrokeEl2.insertAdjacentHTML("afterend", svgStrokeMarkup);
+  }
 });
