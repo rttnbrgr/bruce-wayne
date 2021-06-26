f230f51b29b48cbf1bf655ae17526ab2d75dc308
Nathan Rothenberger <nroth10@gmail.com>
2021-06-26 13:06:17 -0500

    refactor js + cleanup markup

--- src/index.js
+++ src/index.js
@@ -64,53 +64,7 @@ function positionSvgStack() {
   console.log("svgBucketRect", svgBucketRect);
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  addSvgStack();
-  positionSvgStack();
-
-  // // Selectors
-  // const svgBucket = document.querySelector(".svg-bucket");
-  // const svgBucketRect = svgBucket.getClientRects()[0];
-  // console.log("svgBucket", svgBucket);
-  // console.log("svgBucketRect", svgBucketRect);
-
-  // const pictureBox1 = document.querySelector(".picture-box--1");
-  // const pictureBox3 = document.querySelector(".picture-box--3");
-
-  // // get bottom value
-  // const pictureBox1Rect = pictureBox1.getClientRects()[0];
-  // console.log("pictureBox1Rect", pictureBox1Rect);
-  // let boxTop = pictureBox1Rect.bottom + "px";
-
-  // const pictureBox3Rect = pictureBox3.getClientRects()[0];
-  // let boxLeft = pictureBox3Rect.right + "px";
-
-  // // Get position for the svg bucket
-
-  // // get picture bucket
-  // let pictureBoxRect = document
-  //   .querySelector(".picture-box__parent")
-  //   .getClientRects()[0];
-  // // let boxTop = pictureBoxRect.y + "px";
-  // // let boxLeft = pictureBoxRect.x + "px";
-
-  // // calculate width
-  // // right edge of box3, and right edge of whole box
-  // let boxWidth = pictureBoxRect.right - pictureBox3Rect.right;
-
-  // // calculate offset padding
-
-  // // let boxWidth = "200px";
-  // console.log("pictureBoxRect", pictureBoxRect);
-  // console.log("boxWidth", boxWidth);
-
-  // svgBucket.style.top = boxTop;
-  // svgBucket.style.left = boxLeft;
-  // svgBucket.style.width = boxWidth + "px";
-  // svgBucket.style.padding = "32px";
-
-  // console.log("svgBucketRect", svgBucketRect);
-
+function caclulateOffset() {
   // calculate offset padding
   let pictureBox = document.querySelector(".picture-box__parent");
   let pictureBoxRect = pictureBox.getClientRects()[0];
@@ -124,6 +78,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
   console.log("verticalOffsetPadding", verticalOffsetPadding);
 
   pictureBox.style.marginBottom = verticalOffsetPadding + "px";
+}
 
+document.addEventListener("DOMContentLoaded", function (event) {
+  addSvgStack();
+  positionSvgStack();
+  caclulateOffset();
   positionSvgStack();
 });
